ec2f9db938c85b322ee4396581aaf1f9dcec4daf
Aldebaran NAO <nao@schwubunti.ICSI.Berkeley.EDU>
2016-02-13 12:53:41 -0800

    make sure all commit is in repository

--- runAll.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-gnome-terminal -e '/bin/bash -c "roslaunch darwin_gazebo darwin_gazebo.launch"' & 
-sleep 10
-gnome-terminal -e '/bin/bash -c "rosrun cqi_ros run_interface.py"' & 
-sleep 5
-gnome-terminal -e '/bin/bash -c "rosrun ros_ecgworkbench run_nlu.py"' 
-
-
